0eabef63d28bb87991a8da9c4f2e6ad69855b3ee
yushi <94179094@qq.com>
2016-12-23 17:08:09 +0800

    增加注释

--- consumer/src/main/java/charles/sc/consumer/service/FooService.java
+++ consumer/src/main/java/charles/sc/consumer/service/FooService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by Charles on 2016/12/7.
  */
-@FeignClient(value = "provider")//微服务的名称
+@FeignClient(value = "provider")//用@FeignClient注解发出http请求，调用微服务的名称
 @RequestMapping("/foo")//微服务的路径
 public interface FooService {
     @RequestMapping(method = RequestMethod.GET)
